6c61fc176fd6a709771688abb158e35fa677ea44
Victoria V <virlan.victoria@gmail.com>
2021-03-03 18:06:49 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.get("/signup", (req,res, next)=>{
 router.get( "/signout", (req, res, next)=> { 
  req.session.destroy(function (err)
  {
-     res.redirect("auth/signin");
+     res.redirect("/auth/signin");
  });
 });
 
@@ -61,9 +61,34 @@ router.post("/signin", async (req, res, next) => {
   
     
         req.flash("success", "Successfully logged in...");
-        res.redirect("/dashboard/index");
+        res.redirect("/dashboard");
       }
     }
   });
+
+  router.post("/signup", async (req, res, next) => {
+    try {
+      const newUser = { ...req.body };
+      const foundUser = await UserModel.findOne({ email: newUser.email });
+  
+      if (foundUser) {
+        req.flash("warning", "Email already registered");
+        res.redirect("/auth/signup");
+      } else {
+        const hashedPassword = bcrypt.hashSync(newUser.password, 10);
+        newUser.password = hashedPassword;
+        await UserModel.create(newUser);
+        req.flash("success", "Congrats ! You are now registered !");
+        res.redirect("/auth/signin");
+      }
+    } catch (err) {
+      let errorMessage = "";
+      for (field in err.errors) {
+        errorMessage += err.errors[field].message + "\n";
+      }
+      req.flash("error", errorMessage);
+      res.redirect("/auth/signup");
+    }
+});
 
   module.exports = router;
